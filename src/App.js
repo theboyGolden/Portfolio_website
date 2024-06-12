f7d1f569175ea7e3b0b46f6af643992d6ffdb13c
theboyGolden <preciousstick15@gmail.com>
2024-06-12 02:11:27 +0000

    progress made- modified the styling of my navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Profile from './PortfolioContainer/Home/Profile'
+import Home from './PortfolioContainer/Home/Home'
+import './App.css'
 
 const App = () => {
   return (
     <div className='App'>
-      <Profile />
+      <Home />
     </div>
   )
 }
---
+++ src/PortfolioContainer/Home/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css'
+
+
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+        <div className='footer-parent'>
+          <img src={require ('../../../assets/Home/shape-bg.png')} alt='image_not_loaded'/>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
